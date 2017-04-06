60cae05e409aad876417f5eb577657cf71724ec5
Tarik Sarac <tarik.sarac@gmail.com>
2017-04-06 16:54:11 +0200

    redux action

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
-// import Header from './common/Header';
+import Header from './common/Header';
 // import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header here ...</p>
-        {/*<Header
-          loading={this.props.loading}
-        />*/}
+       <Header />
         {this.props.children}
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/homePage';
 import AboutPage from './components/about/aboutPage';
-// import CoursesPage from './components/course/CoursesPage';
+import CoursesPage from './components/course/CoursesPage';
 // import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    {/*<Route path="courses" component={CoursesPage} />*/}
+    <Route path="courses" component={CoursesPage} />
     {/*<Route path="course" component={ManageCoursePage} />*/}
     {/*<Route path="course/:id" component={ManageCoursePage} />*/}
     <Route path="about" component={AboutPage} />
