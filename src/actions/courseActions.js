1194e91b9384ccdf11dc3b279f28eeba284baaac
Tarik Sarac <tarik.sarac@gmail.com>
2017-04-07 15:06:10 +0200

    redux reducer form ..

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,36 @@
-export function createCourse(course) {
-  return { type:'CREATE_COURSE', course};
+import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(course) {
+  return { type: types.LOAD_COURSES_SUCCESS, course};
+}
+
+export function createCourseSuccess(course) {
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course) {
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => { throw(error)});
+  }
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    // dispatch(beginAjaxCall());
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      // dispatch(ajaxCallError(error));
+      throw(error);
+    });
+  };
 }
 
